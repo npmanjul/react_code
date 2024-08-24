e4d060525803b98e8efc7d26c29d07021898246f
Anjul Singh <118563318+npmanjul@users.noreply.github.com>
2024-08-24 13:12:10 +0530

    Update App.jsx

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -12,9 +12,7 @@ function App() {
   const addValue = () => {
     //counter = counter + 1
     setCounter(prevCounter => prevCounter + 1)
-    setCounter(prevCounter => prevCounter + 1 )
-    setCounter(prevCounter => prevCounter + 1)
-    setCounter(prevCounter => prevCounter + 1)
+
     
   }
 
